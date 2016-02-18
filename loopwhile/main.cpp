bd18ad91bfc1d08571779dccb556116a8956cb92
EvelyneBerrios <evelyne.berrios@hotmail.com>
2016-02-18 17:56:01 -0600

    Programa 16

---
+++ loopwhile/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+//Ingresar un numero entero y fijnalizar cuando el numero sea negaivo
+int main()
+{   int numero;
+    do
+    {cout<<"Ingresar el numero";
+    cin>>numero;
+
+
+    }while(numero>0);
+    return 0;
+}
